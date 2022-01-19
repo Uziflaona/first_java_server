289a037fa57df1f7f173eadb3a54a043dae48dcb
Uziflaona <kobraneses@gmail.com>
2022-01-19 18:03:35 +0900

    make it working

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,18 +3,25 @@ package main;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.FirstServlet;
+import templater.PageGenerator;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Exception {
+        FirstServlet firstServlet = new FirstServlet();
+
         Server server = new Server(8080);
         ServletContextHandler context =
                 new ServletContextHandler(ServletContextHandler.SESSIONS);
-        server.setHandler(context);
-//        context.addServlet(new ServletHolder(frontend)), '/authform');
 
-//        server.start();
-//        server.join();
+        ServletHolder servletHolder = new ServletHolder(firstServlet);
+        context.addServlet(servletHolder, "/authform");
+
+        server.setHandler(context);
+
+        server.start();
+        server.join();
     }
 }
